771dd7d7009498253c858acada86379f20ee7bdc
MaS <mas@MaSs-MacBook-Pro.local>
2021-12-22 06:55:13 +0200

    Finished Project

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,31 @@
 function handleSubmit(event) {
-    event.preventDefault()
+  event.preventDefault();
 
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    checkForName(formText)
+  // check what text was put into the form field
+  let formText = document.getElementById("name").value;
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+  console.log("::: Form Submitted :::");
+  fetch("http://localhost:8081/api", {
+    method: "POST",
+    body: JSON.stringify({
+      text: formText,
+    }),
+    headers: {
+      "Content-type": "application/json",
+    },
+  }).then((res) =>
+    res.json().then((data) => {
+      console.log(data);
+      // document.getElementById("results").innerHTML = JSON.stringify(data);
+      document.getElementById("data-model").innerHTML = data.model;
+      document.getElementById("data-score-tag").innerHTML = data.score_tag;
+      document.getElementById("data-agreement").innerHTML = data.agreement;
+      document.getElementById("data-subjectivity").innerHTML =
+        data.subjectivity;
+      document.getElementById("data-confidence").innerHTML = data.confidence;
+      document.getElementById("data-irony").innerHTML = data.irony;
     })
+  );
 }
 
-export { handleSubmit }
+export { handleSubmit };
--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,23 @@
-var path = require("path");
+const path = require("path");
 const express = require("express");
-const mockAPIResponse = require("./mockAPI.js");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const dotenv = require("dotenv");
+const FormData = require("form-data");
+const fetch = require("node-fetch");
+const res = require("express/lib/response");
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+dotenv.config();
 
 app.use(express.static("dist"));
 
 console.log(__dirname);
+console.log(`Your API key is ${process.env.API_KEY}`);
 
 app.get("/", function (req, res) {
   // res.sendFile("dist/index.html");
@@ -14,10 +25,32 @@ app.get("/", function (req, res) {
 });
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-  console.log("Example app listening on port 8080!");
+app.listen(8081, function () {
+  console.log("FEND-NLP app listening on port 8081!");
 });
 
-app.get("/test", function (req, res) {
-  res.send(mockAPIResponse);
+app.post("/api", function (req, res) {
+  const formdata = new FormData();
+  formdata.append("key", process.env.API_KEY);
+  formdata.append("txt", req.body.text);
+  formdata.append("lang", "en"); // 2-letter code, like en es fr ...
+
+  const requestOptions = {
+    method: "POST",
+    body: formdata,
+    redirect: "follow",
+  };
+
+  const response = fetch(
+    "https://api.meaningcloud.com/sentiment-2.1",
+    requestOptions
+  )
+    .then((response) =>
+      response.json().then((data) => ({
+        status: data.status,
+        body: data,
+      }))
+    )
+    .then(({ status, body }) => res.send(body))
+    .catch((error) => console.log("error", error));
 });
